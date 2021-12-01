d2438a62f5804a1ba5982ca2d163dc8eecb8979c
Grzegorz Dlugoszewski <g.dlugoszewski@gmail.com>
2021-12-01 13:49:15 +0100

    Shuffle the whole set instead of randomizing each pull, show set progress

--- app/src/main/java/dev/grdl/lalamido/MainActivity.kt
+++ app/src/main/java/dev/grdl/lalamido/MainActivity.kt
@@ -5,18 +5,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+    var minorScalesAll =  listOf("a", "e", "b", "f#", "c#", "g#", "d#", "d", "g", "c", "f", "bb")
+    var practiceSet = mutableListOf<String>()
+    var setSize = 12
+
     fun randomize(view: View) {
-        val minorScales = listOf("a", "e", "b", "f#", "c#", "g#", "d#", "d", "g", "c", "f", "bb")
-
         val parent: View = view.parent as View
+        val scaleField = parent.findViewById<TextView>(R.id.scaleName)
+        val progressField = parent.findViewById<TextView>(R.id.progress)
 
-        val scale = parent.findViewById<TextView>(R.id.scaleName)
-        scale.text = minorScales.random()
+
+        if (practiceSet.isEmpty()) {
+            practiceSet = minorScalesAll.toMutableList()
+            practiceSet.shuffle()
+        }
+
+        progressField.text = "${practiceSet.size} / ${setSize}"
+
+        val scale = practiceSet.removeLast()
+
+        scaleField.text = scale
+
     }
 }
